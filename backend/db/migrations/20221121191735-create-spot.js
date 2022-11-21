55370aad4ad75a1304a884dc2719b33f734a26fa
kirin <kirinagcaoili@gmail.com>
2022-11-21 15:20:46 -0800

    added options object to migrations drop for spots, bookings, reviews, spot images, and review images

--- backend/db/migrations/20221121191735-create-spot.js
+++ backend/db/migrations/20221121191735-create-spot.js
@@ -71,6 +71,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Spots');
+    await queryInterface.dropTable('Spots', options);
   }
 };
--- backend/db/migrations/20221121193654-create-review-image.js
+++ backend/db/migrations/20221121193654-create-review-image.js
@@ -37,6 +37,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('ReviewImages');
+    await queryInterface.dropTable('ReviewImages', options);
   }
 };
