bd94b89129307eed7b538439c626f4936debf1ca
kirin <kirinagcaoili@gmail.com>
2022-11-23 11:16:42 -0800

    add delete image for spot

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,6 +5,7 @@ const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const bookingsRouter = require('./bookings.js');
 const reviewsRouter = require('./reviews.js');
+const spotImagesRouter = require('./spot-images')
 const { restoreUser } = require('../../utils/auth.js');
 
 // Connect restoreUser middleware to the API router
@@ -21,6 +22,8 @@ router.use('/spots', spotsRouter);
 router.use('/bookings', bookingsRouter)
 
 router.use('/reviews', reviewsRouter);
+
+router.use('/spot-images', spotImagesRouter)
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
---
+++ backend/routes/api/spot-images.js
@@ -0,0 +1,43 @@
+const express = require("express");
+
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const { User, Spot, Review, SpotImage, ReviewImage, Booking } = require("../../db/models");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+const { getMaxListeners } = require("../../app");
+const { route } = require("./users");
+
+const router = express.Router();
+
+// Delete existing image for a spot
+router.delete('/:imageId', requireAuth, async (req, res, next) => {
+    let { imageId } = req.params
+
+    const spotImg = await SpotImage.findByPk(imageId)
+
+    if(!spotImg){
+        return res.status(404).json({
+            message: "Spot Image couldn't be found",
+            statusCode: 404
+        })
+    } 
+    
+    const spot = await Spot.findByPk(spotImg.spotId)
+
+    if(spot.ownerId !== req.user.id){
+        return res.status(403).json({
+            message: "Unauthorized user",
+            statusCode: 403
+          })
+    } else {
+        spotImg.destroy()
+    }
+    
+    return res.status(200).json({
+        message: "Successfully deleted",
+        statusCode: 200
+      })
+})
+
+
+module.exports = router;
