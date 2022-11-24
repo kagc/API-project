097fb7c059abc8864231aa2ab8c118e3125e7203
kirin <kirinagcaoili@gmail.com>
2022-11-23 16:20:34 -0800

    add query filters to get all spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -4,6 +4,7 @@ const { setTokenCookie, requireAuth } = require("../../utils/auth");
 const { User, Spot, Review, SpotImage, ReviewImage, Booking } = require("../../db/models");
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
+const { Op } = require('sequelize');
 // const { getMaxListeners } = require("../../app");
 const { route } = require("./users");
 
@@ -80,21 +81,85 @@ router.post("/", requireAuth, validateSpot, async (req, res, next) => {
 });
 
 // Get all spots
-router.get("/", async (req, res, next) => {
+router.get("/",  async (req, res, next) => {
     let { page, size, minLat, maxLat, minLng, maxLng, minPrice, maxPrice } = req.query
 
+    let errors = {}
+    let errorMarker = false
+   
+    if(page && page < 1){
+        errors.page = "Page must be greater than or equal to 1"
+        errorMarker = true
+    }if(page && !Number.isInteger(parseInt(page))){
+        errors.page = "Page must be greater than or equal to 1"
+        errorMarker = true
+    } if(size && size < 1){
+        errors.size = "Size must be greater than or equal to 1"
+        errorMarker = true
+    }if(size && !Number.isInteger(parseInt(size))){
+        errors.size = "Size must be greater than or equal to 1"
+        errorMarker = true 
+    }if (maxLat && maxLat.indexOf('.') === -1){
+        errors.maxLat = "Maximum latitude is invalid"
+        errorMarker = true
+    }if(maxLat && !Number.isInteger(parseInt(maxLat))){
+        errors.minLat = "Maximum latitude is invalid"
+        errorMarker = true 
+    }if(minLat && !Number.isInteger(parseInt(minLat))){
+        errors.minLat = "Minimum latitude is invalid"
+        errorMarker = true 
+    }if (minLat && minLat.indexOf('.') === -1){
+        errors.minLat = "Minimum latitude is invalid"
+        errorMarker = true
+    }if (minLng && minLng.indexOf('.') === -1){
+        errors.minLng = "Minimum longitude is invalid"
+        errorMarker = true
+    }if(minLng && !Number.isInteger(parseInt(minLng))){
+        errors.minLng = "Minimum longitude is invalid"
+        errorMarker = true 
+    }if (maxLng && maxLng.indexOf('.') === -1){
+        errors.maxLng = "Maximum longitude is invalid"
+        errorMarker = true
+    }if(maxLng && !Number.isInteger(parseInt(maxLng))){
+        errors.maxLng = "Maximum longitude is invalid"
+        errorMarker = true 
+    }if(minPrice && parseInt(minPrice) <= 0){
+        errors.minPrice = "Minimum price must be greater than or equal to 0"
+        errorMarker = true 
+    }if(minPrice && !Number.isInteger(parseInt(minPrice))){
+        errors.minPrice = "Minimum price must be greater than or equal to 0"
+        errorMarker = true 
+    }if(maxPrice && parseInt(maxPrice) <= 0){
+        errors.maxPrice = "Maximum price must be greater than or equal to 0"
+        errorMarker = true 
+    }if(maxPrice && !Number.isInteger(parseInt(maxPrice))){
+        errors.maxPrice = "Maximum price must be greater than or equal to 0"
+        errorMarker = true 
+    }
+
+    if(errorMarker === true){
+        return res.status(400).json({
+            message: "Validation Error",
+            "statusCode": 400,
+            errors
+          })
+    }
+    
     if(!page){
         page = 1
     }
     if(page >= 10){
         page = 10
     }
+    
     if(!size || size >= 20){
         size = 20
     }
     let pagination = {}
+
     page = parseInt(page)
     size = parseInt(size)
+
     if(page >= 1 && size >= 1){
         pagination.limit = size,
         pagination.offset = size*(page-1)
@@ -102,8 +167,53 @@ router.get("/", async (req, res, next) => {
 
     let where = {}
     
+    if(maxLat && minLat){
+        where.lat = {
+            [Op.between]: [minLat, maxLat]
+        }
+    }
+    if(maxLat && !minLat){
+        where.lat = {
+            [Op.lte]: maxLat
+        }
+    }if(minLat && !maxLat){
+        where.lat = {
+            [Op.gte]: minLat
+        }
+    }
 
+    if(maxLng && minLng){
+        where.lng = {
+            [Op.between]: [minLng, maxLng]
+        }
+    }
+    if(maxLng && !minLng){
+        where.lng = {
+            [Op.lte]: maxLng
+        }
+    }if(minLng && ! maxLat){
+        where.lng = {
+            [Op.gte]: minLng
+        }
+    }
+
+    if(maxPrice && minPrice){
+        where.price = {
+            [Op.between]: [minPrice, maxPrice]
+        }
+    }
+    if(maxPrice && !minPrice){
+        where.price = {
+            [Op.lte]: maxPrice
+        }
+    }if(minPrice && !maxPrice){
+        where.price = {
+            [Op.gte]: minPrice
+        }
+    }
+console.log(where)
   let spots = await Spot.findAll({
+    where,
     include: [{
         model: Review
     },{
