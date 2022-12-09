2981633c740fcdfc6f8d82970e138349258290a9
kirin <kirinagcaoili@gmail.com>
2022-12-09 13:58:15 -0800

    add navigation

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -34,7 +34,7 @@ router.post(
         const err = new Error('Login failed');
         err.status = 401;
         err.title = 'Invalid credentials';
-        // err.errors = ['The provided credentials were invalid.'];
+        err.errors = ['The provided credentials were invalid.'];
         return next(err);
       }
   
