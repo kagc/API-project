8cc080e030ef4578c78483fc365df1e8be0b515a
kirin <kirinagcaoili@gmail.com>
2022-12-16 19:31:45 -0800

    more styling for details page

--- frontend/src/components/SingleSpot/index.js
+++ frontend/src/components/SingleSpot/index.js
@@ -120,15 +120,51 @@ function SingleSpot() {
             </div>
 <div className='infos'>
             <div className='leftbox'>
-                <h2>Spot hosted by {spot.Owner.firstName}</h2>
+                <h2 className='hostedby-text'>Spot hosted by {spot.Owner.firstName}</h2>
+                <p className='subhostedby-text'>4 cats · 1 pillow · 1,000 cat hairs</p>
+
+<div className='extra-infobox'>
+    <div className='extra-deets'>
+    <i className="fa-solid fa-shield-cat"></i>
+    <div className='more-deets'>
+        <h4 className='bold-deet'>Accidental damage protection</h4>
+        <span className='subdeet'>Because cats.</span>
+    </div>
+    </div>
+
+    <div className='extra-deets'>
+    <i class="fa-solid fa-paw"></i>
+    <div className='more-deets'>
+        <h4 className='bold-deet'>Pet-friendly!</h4>
+        <span className='subdeet'>Also because cats.</span>
+    </div>
+    </div>
+
+    <div className='extra-deets'>
+    <i class="fa-regular fa-calendar"></i>
+    <div className='more-deets'>
+        <h4 className='bold-deet'>Free cancellation for 48 hours.</h4>
+        <span className='subdeet'>You'll miss out on the cats, though.</span>
+    </div>
+    </div>
+
+
+</div>
+
+<div className='extra-infobox'>
+<img className='aircover' src='https://a0.muscache.com/im/pictures/54e427bb-9cb7-4a81-94cf-78f19156faad.jpg' ></img>
+<span className='subdeet2'>Every booking includes a thick blanket to cover yourself with for when the cats have their scheduled 3AM zoomies.</span>
+
+</div>
 
                 <div className='descr'>
 
 <span className='descr2'>{spot.description}</span>
 </div>
-            </div>
 
-            <div>
+            </div>
+                <div className='another-div'>
+                    <div className='floaty-box-holder'>
                 <div className='floaty-box'>
                     <div className='topline'>
                     <div>
@@ -158,6 +194,8 @@ function SingleSpot() {
                 
                 </div>
             </div>
+                </div>
+            
 </div>
            
 
